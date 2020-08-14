7b73c82c8817173d65922cb70c390c2fcdaaf0d3
jingdd-boop <1736929925@qq.com>
2020-08-14 10:00:46 +0800

    vue

--- service/index.js
+++ service/index.js
@@ -1,10 +1,31 @@
 const Koa = require('koa')
-const app =new Koa()
+const app = new Koa()
+const mongoose = require('mongoose')
+const {connect , initSchemas} = require('./database/init.js')
+
+//立即执行函数
+;(async () =>{
+    await connect()
+    initSchemas()
+    const User = mongoose.model('User')
+    let oneUser = new User({userName:'jspang13',password:'123456'})
+
+    oneUser.save().then(()=>{
+        console.log('插入成功')
+
+    })
+let  users = await  User.findOne({}).exec()
+
+console.log('------------------')
+console.log(users)
+console.log('------------------')  
+})()
+
 
 app.use(async(ctx)=>{
-  ctx.body='<h1>hello</h1>'
+    ctx.body = '<h1>hello Koa2</h1>'
 })
 
 app.listen(3000,()=>{
-  console.log('[server] star')
+    console.log('[Server] starting at port 3000')
 })
